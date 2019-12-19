e8d6c812b4453c35e78903131831d1f67a6a513c
LucySpass <ivana.begovic@yahoo.com>
2019-12-19 23:54:21 +0300

    changed price to rubles

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+""" Два базовых класса """
+
+
 class Beverage:
     _description = 'Unknown'
 
@@ -26,31 +29,37 @@ class Decorator(Beverage):
         return self._component.cost()
 
 
+""" Два основных класса """
+
+
 class Espresso(Beverage):
     def get_description(self) -> str:
         return 'Espresso'
 
     def cost(self) -> float:
-        return 1.89
+        return 90
 
 
-class HouseBlend(Beverage):
+class BlackTea(Beverage):
     def get_description(self) -> str:
         return 'House Blend'
 
     def cost(self) -> float:
-        return .89
+        return 40
 
 
-class Mocha(Decorator):
+""" Четыре класса - декоратора """
+
+
+class Syrup(Decorator):
     def __init__(self, component: Beverage) -> None:
         super().__init__(component)
 
     def get_description(self) -> str:
-        return super().component.get_description() + ', Mocha'
+        return super().component.get_description() + ', Syrup'
 
     def cost(self) -> float:
-        return .40 + super().component.cost()
+        return 30 + super().component.cost()
 
 
 class WhippedCream(Decorator):
@@ -61,7 +70,7 @@ class WhippedCream(Decorator):
         return super().component.get_description() + ', Whipped Cream'
 
     def cost(self) -> float:
-        return .20 + super().component.cost()
+        return 20 + super().component.cost()
 
 
 class Milk(Decorator):
@@ -72,7 +81,7 @@ class Milk(Decorator):
         return super().component.get_description() + ', Milk'
 
     def cost(self) -> float:
-        return .25 + super().component.cost()
+        return 30 + super().component.cost()
 
 
 class Sugar(Decorator):
@@ -90,20 +99,20 @@ if __name__ == "__main__":
     stop_coffee = False
     print('Welcome to our cozy cafe Decoracofe!')
     print('Your hosts are Ivana Begovic and Svetlana Ruchkina.')
-    print('Please, chose your coffee.')
-    bev_type = input('Espresso(1) or Home blend(2)?')
+    print('Please, chose your beverage.')
+    bev_type = input('Espresso(1) or Black tea(2)?')
     beverage = Espresso()
     if bev_type == "1":
         beverage = Espresso()
     else:
-        beverage = HouseBlend()
+        beverage = BlackTea()
     while not stop_coffee:
         print('Please, chose additional components:')
         add = input('That`s it, thank you(0) '
                     '\n Sugar, please (1) '
                     '\n Whipped cream, please(2) '
                     '\n Milk, please(3) '
-                    '\n Mocha, please(4)')
+                    '\n Syrup, please(4):')
         if add == "0":
             stop_coffee = True
         elif add == "1":
@@ -113,7 +122,7 @@ if __name__ == "__main__":
         elif add == "3":
             beverage = Milk(beverage)
         elif add == "4":
-            beverage = Mocha(beverage)
+            beverage = Syrup(beverage)
 
     print('Thank you for choosing us! \n Your order is:', beverage.get_description(),
-          '\n Your total is:', beverage.cost())
+          '\n Your total is:', beverage.cost(), 'rubles.')
